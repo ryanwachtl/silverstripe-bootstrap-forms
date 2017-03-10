dba294b939211aa079fa250149fc77c850fa1c83
Ryan Wachtl <ryan.wachtl@gmail.com>
2017-03-10 14:09:13 -0600

    Use $this->owner in updateForm
    Extension is now applied directly to UserForm

--- code/BootstrapUserForm.php
+++ code/BootstrapUserForm.php
@@ -1,12 +1,9 @@
 <?php
 
-
-
 class BootstrapUserForm extends Extension {
 
-
-
-	public function updateForm($form) {
+	public function updateForm() {
+        $form = $this->owner;
 		$form->Fields()->bootstrapify();
 		$form->Actions()->bootstrapify();
 		$form->setTemplate("BootstrapForm");

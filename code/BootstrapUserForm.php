27c7a771aaaff1037567c0daf5c02290b15b01db
Ryan Wachtl <ryan.wachtl@gmail.com>
2017-03-23 15:25:28 -0500

    Update BootstrapUserForm.php

--- code/BootstrapUserForm.php
+++ code/BootstrapUserForm.php
@@ -3,10 +3,16 @@
 class BootstrapUserForm extends Extension {
 
 	public function updateForm() {
-        $form = $this->owner;
-		$form->Fields()->bootstrapify();
-		$form->Actions()->bootstrapify();
+		$form = $this->owner;
 		$form->setTemplate("BootstrapForm");
+	}
+
+	public function updateFormFields($fields) {
+		$fields->bootstrapify();
+	}
+
+	public function updateFormActions($actions) {
+		$actions->bootstrapify();
 	}
 
 }
